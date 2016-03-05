892dae7b880b980355d1fcfaff53bb43d8cf8769
Byunghyun-P <park@byunghyun.me>
2016-03-05 15:21:09 +0900

    emit every time when user || room changed

--- server/app.js
+++ server/app.js
@@ -34,7 +34,7 @@ console.log("http server listening on %d", port);
 
 var rooms = [];
 io.on('connection', function(socket){
-  console.log('a user connected');
+  io.emit('roomInformation', io.sockets.adapter.rooms);
 
   var roomID = 'global';
   socket.join(roomID);
@@ -42,13 +42,13 @@ io.on('connection', function(socket){
   socket.on('joinRoom', function(data){
     roomID = data;
     socket.join(roomID);
-    console.log('JOIN ROOM LIST', io.sockets.adapter.rooms);
+    io.emit('roomInformation', io.sockets.adapter.rooms);
   });
 
   socket.on('leaveRoom', function(data){
     socket.leave(roomID);
     roomID = 'global';
-    console.log('OUT ROOM LIST', io.sockets.adapter.rooms);
+    io.emit('roomInformation', io.sockets.adapter.rooms);
   });
 
   socket.on('roomInformation', function(){
@@ -61,5 +61,9 @@ io.on('connection', function(socket){
 
   socket.on('typing', function(bool){
     io.emit('typing', bool)
+  });
+
+  socket.on('disconnect', function(){
+    io.emit('roomInformation', io.sockets.adapter.rooms);
   });
 });
