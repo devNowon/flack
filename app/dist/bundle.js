cb25aa0638470d5e17a1ad47f85e36ca18265f02
Byunghyun-P <park@byunghyun.me>
2016-03-05 12:50:26 +0900

    changed bundle

--- app/dist/bundle.js
+++ app/dist/bundle.js
@@ -19699,7 +19699,7 @@
 
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-	var SOCKET = (0, _socket2.default)('localhost:3000');
+	var SOCKET = (0, _socket2.default)('http://murmuring-ridge-75162.herokuapp.com/');
 
 	var MessageWrapperComponent = function (_React$Component) {
 	  _inherits(MessageWrapperComponent, _React$Component);
@@ -19739,7 +19739,6 @@
 	  }, {
 	    key: 'getRoomInformation',
 	    value: function getRoomInformation() {
-	      console.log('asd');
 	      SOCKET.emit('roomInformation');
 	    }
 	  }, {
