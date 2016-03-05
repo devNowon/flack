6de34e582d249031d89d173f37e5b33fb90dd94d
garam <studio@gar.am>
2016-03-05 12:38:53 +0900

    add superagent

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var http = require("http");
 var express = require("express");
 var app = express();
+var request = require('superagent');
 var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/app"));
@@ -17,9 +18,9 @@ app.get('/channel', function(req, res) {
   };
   res.send(data);
 });
-
+var user =[];
 app.get('/user/list', function(req,res) {
-  var user = [
+  user = [
     {
       id : 'id',
       name : 'name',
@@ -42,8 +43,22 @@ console.log("http server listening on %d", port);
 
 io.on('connection', function(socket){
   console.log('a user connected');
+    console.log(socket.id);
+    // console.log(io.of('/').clients());
+    io.emit('userID', socket.id);
+  console.log(io.engine.clientsCount);
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+    console.log(user);
+  });
+
+  socket.on('private message', function(msg){
+    io.emit('private message', msg);
+  });
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+    // user.push({id : 'id',name : socket.id});
   });
   socket.on('typing', function(bool){
     io.emit('typing', bool);
