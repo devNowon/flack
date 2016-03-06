5f2d233a8b72b4a889f4cec3c81b4ad6aeebc7b1
WoongheeLee <eunvanz@naver.com>
2016-03-06 21:49:33 +0900

    아까 커밋 안된게 있었음...

--- app/asset/react_components/side_components/SideItemWrapper.jsx
+++ app/asset/react_components/side_components/SideItemWrapper.jsx
@@ -4,7 +4,6 @@ import Menu from 'material-ui/lib/menus/menu';
 import FontIcon from 'material-ui/lib/font-icon';
 
 const style = {
-<<<<<<< HEAD
   menu: {
     // 메뉴 스타일 작성
     maxWidth: '300px'
@@ -40,45 +39,6 @@ class SideItemWrapper extends React.Component {
         } 
       }
       return itemDom;
-=======
-	menu : {
-		// 메뉴 스타일 작성
-	}
-};
-
-class SideItemWrapper extends React.Component {
-    constructor(props) {
-        super(props);
-        this.displayName = 'SideItemWrapper';
-    }
-    render() {
-        "use strict";
-    	// 채널 리스트를 뿌려주는 메소드
-    	let showChannelList = () => {
-    		let itemDom = [];
-    		let channelItems = this.props.channelArr; // 채널 이름 배열
-            console.log('channelItems : ' + this.props.channelArr);
-    		if (channelItems.length > 0) {
-               for (let item of channelItems) {
-                    itemDom.push(<SideItem 
-                    key={item}
-                    icon={<FontIcon
-                              className="material-icons face"
-                            />} 
-                    name={item}
-                    handleChnlItemClick={this.props.handleChnlItemClick}></SideItem>);
-                } 
-            }
-    		return itemDom;
-    	}
-        return (
-        	<div>
-	        	<Menu style={style.menu}>
-	        		{ showChannelList() }
-	        	</Menu>
-        	</div>
-        );
->>>>>>> 623b177fb5fa044898d4d024fc78a6e09d123ac8
     }
     return (
       <div>
