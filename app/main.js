867203392f94dc7531bc6fd31cac230929e6e4fe
WoongheeLee <eunvanz@naver.com>
2016-03-13 15:02:37 +0900

    view 쪽에 몇 가지 업데이트 - 전체 레이아웃 컴포넌트 생성 (AppWrapper) - 전체 레이아웃을 크게 side와 content로 나눔 (SideWrapper, ContentWrapper) - 채널 선택과 회원 목록 side에 출력, 나머지는 content에 출력

--- app/main.js
+++ app/main.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 import MessageWrapperComponent from './asset/react_components/MessageWrapperComponent.jsx';
 import ListWrapperComponent from './asset/react_components/ListWrapperComponent.jsx';
+import AppWrapper from './asset/react_components/AppWrapper.jsx';
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 // Needed for onTouchTap
@@ -11,6 +12,8 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 // https://github.com/zilverline/react-tap-event-plugin
 injectTapEventPlugin();
 
-ReactDOM.render(<ListWrapperComponent />, document.getElementById('listPeoples'));
-ReactDOM.render(<MessageWrapperComponent />, document.getElementById('contents'));
+// ReactDOM.render(<ListWrapperComponent />, document.getElementById('listPeoples'));
+// ReactDOM.render(<MessageWrapperComponent />, document.getElementById('contents'));
+ReactDOM.render(<AppWrapper />, document.getElementById('app'));
+
  
