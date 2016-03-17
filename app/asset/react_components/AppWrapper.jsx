936332ae45bffb80907e390cfc8a855fecef1284
WoongheeLee <eunvanz@naver.com>
2016-03-18 00:30:42 +0900

    채널 생성 다이얼로그 채널 이름 중복 체크 구현 - 중복된 이름일 경우 메시지 출력

--- app/asset/react_components/AppWrapper.jsx
+++ app/asset/react_components/AppWrapper.jsx
@@ -3,6 +3,10 @@ import SideWrapper from './SideWrapper.jsx';
 import ContentWrapper from './ContentWrapper.jsx';
 import CreateChannelFormDialog from './content_components/CreateChannelFormDialog.jsx';
 import MessageWrapperComponent from './MessageWrapperComponent.jsx';
+import io from 'socket.io-client';
+import _ from 'lodash';
+
+const SOCKET = io('http://murmuring-ridge-75162.herokuapp.com/');
 
 class AppWrapper extends React.Component {
   constructor(props) {
@@ -11,6 +15,7 @@ class AppWrapper extends React.Component {
     this.state = {
       contentComponent: MessageWrapperComponent,
       openCreateChannelForm: false,
+      channelArr: [],
     };
     this.handleChnlAddClick = this.handleChnlAddClick.bind(this);
     this.handleChnlItemClick = this.handleChnlItemClick.bind(this);
@@ -29,6 +34,18 @@ class AppWrapper extends React.Component {
   handleCloseCreateChannelForm() {
     this.setState({openCreateChannelForm: false});
   }
+  componentWillMount() {
+    // 채널 리스트 받아오는 코드
+    this._getChnlInformation();
+  }
+  _getChnlInformation() {
+    SOCKET.on('roomInformation', (obj) => {
+      const chnlObj =  obj;
+      const result = _.keys(chnlObj).filter((key) => !(_.startsWith(_.trim(key), '/#')));
+      this.setState({channelArr: result});
+    });
+    SOCKET.emit('roomInformation');
+  }
   render() {
     return (
       <div>
@@ -39,12 +56,14 @@ class AppWrapper extends React.Component {
         <SideWrapper
           handleChnlAddClick={this.handleChnlAddClick}
           handleChnlItemClick={this.handleChnlItemClick}
+          channelArr={this.state.channelArr}
         />
         <CreateChannelFormDialog
           modal={false}
           open={this.state.openCreateChannelForm}
           onRequestClose={this.handleCloseCreateChannelForm}
           handleClose={this.handleCloseCreateChannelForm}
+          channelArr={this.state.channelArr}
         />
       </div>
     );
--- app/asset/react_components/channel_components/ChannelWrapperComponent.jsx
+++ app/asset/react_components/channel_components/ChannelWrapperComponent.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import io from 'socket.io-client';
-import _ from 'lodash';
-
 import SideItemWrapper from '../common_components/SideItemWrapper.jsx';
 import SideAppBar from '../common_components/SideAppBar.jsx';
 import FontIcon from 'material-ui/lib/font-icon';
-
-const SOCKET = io('http://murmuring-ridge-75162.herokuapp.com/');
 
 const style = {
   leftIcon: {
@@ -18,32 +13,17 @@ class ChannelWrapperComponent extends React.Component {
   constructor(props) {
     super(props);
     this.displayName = 'ChannelWrapperComponent';
-    this.state = {
-      channelArr: [],
-    };
-  }
-  componentWillMount() {
-    // 채널 리스트 받아오는 코드
-    this._getChnlInformation();
-  }
-  _getChnlInformation() {
-    SOCKET.on('roomInformation', (obj) => {
-      const CHNL_OBJ =  obj;
-      const result = _.keys(CHNL_OBJ).filter((key) => !(_.startsWith(_.trim(key), '/#')));
-      this.setState({channelArr: result});
-    });
-    SOCKET.emit('roomInformation');
   }
   render() {
     return(
      <div>
         <SideAppBar
-          itemLength={this.state.channelArr.length}
+          itemLength={this.props.channelArr.length}
           title="CHANNELS"
           addToolTip="Add Channel"
           handleAddClick={this.props.handleChnlAddClick}/>
         <SideItemWrapper
-          itemArr={this.state.channelArr}
+          itemArr={this.props.channelArr}
           leftIcon={<FontIcon
               className="fa fa-hashtag fa-1"
               style={style.leftIcon}
