3dd6d2db8eb58b16de8706e762fcab47480ffc06
Byunghyun-P <park@byunghyun.me>
2016-03-19 13:43:38 +0900

    change structure of project

---
+++ tools/deploy.js
@@ -0,0 +1,19 @@
+
+var argv = require('optimist').argv;
+
+if(argv.process == 'before'){
+     require('simple-git')(require("path").resolve("./build"))
+     //.init()
+     //.addRemote('origin', 'https://git.heroku.com/murmuring-ridge-75162.git')
+     .pull('origin','master');
+} else {
+     require('simple-git')(require("path").resolve("./build"))
+     .add('./*')
+     .commit("c")
+     .push('origin', 'master')
+     .then(function(err){
+      if(err)
+        console.log(err)
+      console.log('deployed')
+     });  
+}
