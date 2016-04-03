afea024a854fa6a40d74fbf392822b81eb408257
garam <studio@gar.am>
2016-04-03 20:48:29 +0900

    Add Model ('user')

--- src/server/model/user.model.js
+++ src/server/model/user.model.js
@@ -2,7 +2,15 @@ var mongoose = require('mongoose');
 var passportLocalMongoose = require('passport-local-mongoose');
 
 // define the schema for our user model
-var userSchema = new mongoose.Schema({});
+var userSchema = new mongoose.Schema({
+  email : String,
+  nickname : String,
+  note : String,
+  channel : Array,
+  teamid : Array,
+},{
+	timestamps: true
+});
 
 userSchema.plugin(passportLocalMongoose, {
   usernameUnique: 'true'
